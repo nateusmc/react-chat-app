45523f8eafd4355f5e8f1979b7cbcd9ce85d9926
Nate Garrett <nate.garrett1992@gmail.com>
2018-01-17 19:18:46 -0600

    got socket.io server connected with a socket id and running server and react app concurrently

---
+++ src/components/Layout.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+
+const socketUrl = 'http://localhost:3000/';
+
+export default class Layout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      socket: null,
+    };
+  }
+
+  componentWillMount() {
+    this.initSocket();
+  }
+
+  initSocket = () => {
+    const socket = io(socketUrl);
+    socket.on('connect', () => {
+      console.log('Connected Successfully');
+    });
+
+    this.setState({ socket });
+  };
+
+  render() {
+    const { title } = this.props;
+    return (
+      <div className='container'>
+        { title }
+      </div>
+    );
+  }
+}
